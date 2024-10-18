41060534fdf718268f8678231b78fe5f2e05acc0
thiagoviks <thiagosudgomes@gmail.com>
2024-10-18 16:47:36 -0300

    Modified dockerfile and added main method to program.cs

--- EcoClean/Program.cs
+++ EcoClean/Program.cs
@@ -3,55 +3,62 @@ using EcoClean.Data.Repository;
 using EcoClean.Services;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-#region INICIALIZANDO O BANCO DE DADOS
-var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
-builder.Services.AddDbContext<DatabaseContext>(
-    opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true)
-);
-#endregion
-
-#region Repositorios
-builder.Services.AddScoped<IColetaRepository, ColetaRepository>();
-builder.Services.AddScoped<IEnderecoRepository, EnderecoRepository>();
-builder.Services.AddScoped<INotificacaoRepository, NotificacaoRepository>();
-builder.Services.AddScoped<IMoradorRepository, MoradorRepository>();
-builder.Services.AddScoped<ICaminhaoRepository, CaminhaoRepository>();
-builder.Services.AddScoped<IRotaRepository, RotaRepository>();
-builder.Services.AddScoped<ITipoResiduoRepository, TipoResiduoRepository>();
-#endregion
-
-#region Services
-builder.Services.AddScoped<IColetaService, ColetaService>();
-builder.Services.AddScoped<IEnderecoService, EnderecoService>();
-builder.Services.AddScoped<INotificacaoService, NotificacaoService>();
-builder.Services.AddScoped<IMoradorService, MoradorService>();
-builder.Services.AddScoped<ICaminhaoService, CaminhaoService>();
-builder.Services.AddScoped<IRotaService, RotaService>();
-builder.Services.AddScoped<ITipoResiduoService, TipoResiduoService>();
-#endregion
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        #region INICIALIZANDO O BANCO DE DADOS
+        var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+        builder.Services.AddDbContext<DatabaseContext>(
+            opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(true)
+        );
+        #endregion
+
+        #region Repositorios
+        builder.Services.AddScoped<IColetaRepository, ColetaRepository>();
+        builder.Services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+        builder.Services.AddScoped<INotificacaoRepository, NotificacaoRepository>();
+        builder.Services.AddScoped<IMoradorRepository, MoradorRepository>();
+        builder.Services.AddScoped<ICaminhaoRepository, CaminhaoRepository>();
+        builder.Services.AddScoped<IRotaRepository, RotaRepository>();
+        builder.Services.AddScoped<ITipoResiduoRepository, TipoResiduoRepository>();
+        #endregion
+
+        #region Services
+        builder.Services.AddScoped<IColetaService, ColetaService>();
+        builder.Services.AddScoped<IEnderecoService, EnderecoService>();
+        builder.Services.AddScoped<INotificacaoService, NotificacaoService>();
+        builder.Services.AddScoped<IMoradorService, MoradorService>();
+        builder.Services.AddScoped<ICaminhaoService, CaminhaoService>();
+        builder.Services.AddScoped<IRotaService, RotaService>();
+        builder.Services.AddScoped<ITipoResiduoService, TipoResiduoService>();
+        #endregion
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
